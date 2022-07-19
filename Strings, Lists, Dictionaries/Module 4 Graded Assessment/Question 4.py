026965903f14665707fd2d36510c98e4bea0670e
Piyush Acharya <acharyapiyush1@outlook.com>
2022-07-18 21:31:09 -0700

    Create Question 4.py

---
+++ Strings, Lists, Dictionaries/Module 4 Graded Assessment/Question 4.py
@@ -0,0 +1,9 @@
+def squares(start, end):
+	squares = []
+	for i in range(start, end + 1):
+		squares.append(i**2)
+	return squares
+
+print(squares(2, 3)) # Should be [4, 9]
+print(squares(1, 5)) # Should be [1, 4, 9, 16, 25]
+print(squares(0, 10)) # Should be [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
